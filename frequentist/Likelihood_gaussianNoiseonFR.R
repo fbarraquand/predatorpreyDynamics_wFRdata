a4674822497e21c00d52f0f1eeed80378021ba1e
frederic <frederic.barraquand@u-bordeaux.fr>
2017-11-13 15:10:12 +0100

    Corrected Likelihood function

--- frequentist/Likelihood_gaussianNoiseonFR.R
+++ frequentist/Likelihood_gaussianNoiseonFR.R
@@ -74,15 +74,91 @@ P_t = exp(y[t-1,2])
 mu1 = y[t-1,1] + theta[1] - log(1+theta[2]*N_t) - y[t-1,3]*N_t/P_t
 mu2 = y[t-1,2] + theta[4] - log((1+theta[5]*P_t/N_t))
 mu3 = (theta[7]*N_t)/(theta[8] + N_t)
-ll= ll + log(dnorm(y[t,1], mu1, theta[3])) +  log(dnorm(y[t,2], mu2, theta[6])) +log(dnorm(y[t,3], mean = mu1, sd = theta[9]))
+#ll= ll + log(dnorm(y[t,1], mu1, theta[3])) +  log(dnorm(y[t,2], mu2, theta[6])) + log(dnorm(y[t,3], mean = mu3, sd = theta[9]))
+# we have log(0) problem
+d1=dnorm(y[t,1], mu1, theta[3],log=T) ## directly asking for the log avoids problems
+d2=dnorm(y[t,2], mu2, theta[6],log=T)
+d3=dnorm(y[t,3], mu3, theta[9],log=T)
+ll=ll+d1+d2+d3
 }
-return(ll)
+return(-ll)
 }
 
 ################ Now optimize the LL ###########################################
-theta_start = c(runif(1,0.5,2),runif(1,0.001,0.01),runif(1,0.1,2),runif(1,0.1,2),runif(1,0.1,5),runif(1,0.1,2), runif(1,10,100),runif(1,0.01,0.1),runif(1,1,10))
-#theta=theta_start
-#y=data
+theta_start = c(runif(1,0.5,2),runif(1,0.001,5),runif(1,0.1,1),runif(1,0.1,2),runif(1,0.1,5),runif(1,0.1,2),runif(1,0.01,0.1),runif(1,10,100),runif(1,0.1,5))
+# Put high values for D not C -- previously the reverse was done for bayesian est. (error?)
+# theta_start=rep(1,9) # you never know...
 
 p_opt<-optim(theta_start, logLik, y=data,method="BFGS",hessian=T)
+p_opt<-optim(theta_start, logLik, y=data,hessian=T)
+# does not work -- unclear why? Use L-BFGS? 
+library(optimx)
+p_opt<-optimx(theta_start, logLik, y=data,hessian=T)
+## Always same warnings -> In log(1 + theta[2] * N_t) : NaNs produced
+
+### Let's try something extreme 
+theta_true  = c(rmax_V,1/K,sqrt(0.05),rmax_P,Q,sqrt(0.05),D,C,sqrt(0.05))
+p_opt<-optim(theta_true, logLik, y=data,method="BFGS",hessian=T)
+p_opt$par
+theta_true
+
+############ Create likelihood profiles ########################################
+
+# basic check 
+logLik(theta_true,data)
+llbis=matrix(0,nrow=10,ncol=10)
+for (i in 1:10){
+  for (j in 1:10){
+    theta_new=theta_true+c(0,0,0,0,0,0,0.05*i-0.5,0.5*j-2,0)
+    llbis[i,j]=logLik(theta_new,data)
+  }
+}
+contour(llbis)
+
+# do that with (r,gamma)
+logLik(theta_true,data)
+llbis=matrix(0,nrow=10,ncol=10)
+r_new=g_new=rep(0,10)
+for (i in 1:10){
+  for (j in 1:10){
+    r_new[i] = 1*i-0.1
+    g_new[j] = 0.2*j-0.9
+    theta_new=theta_true+c(r_new[i],g_new[j],0,0,0,0,0,0)
+    llbis[i,j]=logLik(theta_new,data)
+  }
+}
+contour(theta_true[1]+r_new,theta_true[2]+g_new,llbis)
+
+# Let's assume we know the max growth rates (most realistic assumption)
+
+gamma = c(0.001,0.01,0.05,0.1,0.5)
+negll=matrix(0,nrow=100,ncol=101)
+
+par(mfrow=c(2,3))
+for (kg in 1:length(gamma)){
+  D_FR = seq(0.5,20.5,by=0.2)
+    for (kd in 1:length(D_FR)){
+      C_FR = seq(0.05,5,0.05) 
+      for (kc in 1:length(C_FR)){
+       theta = c(rmax_V,gamma[kg],sqrt(0.05),rmax_P,Q,sqrt(0.05),C_FR[kc],D_FR[kg],sqrt(0.05))
+       negll[kc,kd]=logLik(theta,data)
+      }
+    }
+  contour(C_FR,D_FR,negll,xlab="C",ylab="D", main=c("Gamma = ",toString(gamma[kg])))
+}
+
+########### Other more realistic profiles #####################################
+
+
+
+
+
+  
+  
+
+### NB Should I normalize everything and get rid of the sigmas? 
+
+
+
+
 
