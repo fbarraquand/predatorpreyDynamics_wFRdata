efbdf8c40414bf3d779eccca13e58c4dc037e3cc
fbarraquand <frederic.barraquand@u-bordeaux.fr>
2020-10-01 10:47:07 +0200

    Frequentist estimators with L-BFGS-B

--- frequentist/parameter_estimators.R
+++ frequentist/parameter_estimators.R
@@ -13,27 +13,14 @@ file_path2 = "../simulations/small_noise_on_FR/parameter_sets/noisy_cycles/T=100
 ### Datasets of the form
 ### data = cbind(log(N),log(P),FR) 
 
-#krep=1 # sample chosen (if one)
-C_est = D_est = C_est_woutFR = D_est_woutFR = rep(NA,100) #Initializing estimators of C and D
-
-for (krep in 1:100){
-
-# loading dataset, perturbed FP
-data1 = read.csv(paste(file_path1,"predatorPrey_withGaussianFR",krep,".csv",sep=""))
-names(data1)=c("Time","n","p","KR")
-data1[,1]<-NULL
-head(data1)
-
-# switch to noisy LC dataset
-data2 = read.csv(paste(file_path2,"predatorPrey_withGaussianFR",krep,".csv",sep=""))
-names(data2)=c("Time","n","p","KR")
-data2[,1]<-NULL
-head(data2)
-
 ## Sourcing the functions to compute the likelihood
 source('../frequentist/LikelihoodFunctions.R')
 source('../figures_article/figlabel.R')
 cex_labels = 1.3
+
+#krep=1 # sample chosen (if one)
+C_est = D_est = C_est_woutFR = D_est_woutFR = rep(NA,100) #Initializing estimators of C and D
+
 
 ### Parameters of the model - fixed point parameter set
 K<-1			# threshold dd 
@@ -48,16 +35,24 @@ Q<-10
 theta_true1  = c(rmax_V,1/K,sqrt(0.05),rmax_P,Q,sqrt(0.05),C,D,sqrt(0.05))
 theta_true2  = c(rmax_V,1/K,sqrt(0.05),rmax_P,Q,sqrt(0.05),C,D)
 
+for (krep in 1:100){
+
+# loading dataset, perturbed FP
+data1 = read.csv(paste(file_path1,"predatorPrey_withGaussianFR",krep,".csv",sep=""))
+names(data1)=c("Time","n","p","KR")
+data1[,1]<-NULL
+head(data1)
+
 ## Now optimizing from somewhere random
 
 theta_start = c(runif(1,0.5,5),runif(1,0.01,10),runif(1,0.05,1),runif(1,0.1,1),runif(1,1,15),runif(1,0.05,1),runif(1,0.01,5),runif(1,1,5),runif(1,0.05,1))
 
-p_opt<-try(optim(theta_start, logLik, y=data1,method="BFGS",hessian=T), silent=TRUE)
+p_opt<-try(optim(theta_start, logLik, y=data1,method="L-BFGS-B",lower = 0.01, hessian=T), silent=TRUE)
 if(is(p_opt, 'try-error'))
 {  
   C_est[krep] = NA
   D_est[krep] = NA 
-  break
+  next
 } else {
   C_est[krep] = p_opt$par[7]
   D_est[krep] = p_opt$par[8]
@@ -68,12 +63,12 @@ if(is(p_opt, 'try-error'))
 ### --------------- Without the KR data ------------------------
 theta_start = theta_start[-9]#one parameter less
 
-p_opt<-try(optim(theta_start, logLik_FRwoutNoise, y=data1,method="BFGS",hessian=T), silent=TRUE)
+p_opt<-try(optim(theta_start, logLik_FRwoutNoise, y=data1,method="L-BFGS-B",lower = 0.01,hessian=T), silent=TRUE)
 if(is(p_opt, 'try-error'))
 {  
   C_est_woutFR[krep] = NA
   D_est_woutFR[krep] = NA 
-  break
+  next
 } else {
   C_est_woutFR[krep] = p_opt$par[7]
   D_est_woutFR[krep] = p_opt$par[8]
@@ -83,31 +78,36 @@ if(is(p_opt, 'try-error'))
 
 } # end of loop on repeats
 
-pdf(file = "Estimators_BFGS_perturbedFP.pdf")
+pdf(file = "Estimators_L-BFGS-B_perturbedFP.pdf")
 par(mfrow=c(2,2))
-hist(C_est,breaks=30,xlim=c(0,50))
+hist(C_est,breaks=30,xlim=c(0,60))
 abline(v=C,col="red",lwd=2)
 hist(D_est,breaks=30,xlim=c(0,60))
 abline(v=D,col="red",lwd=2)
 
-hist(C_est_woutFR,breaks=30)
+hist(C_est_woutFR,breaks=30,xlim=c(0,60))
 abline(v=C,col="red",lwd=2)
-hist(D_est_woutFR,breaks=30)
+hist(D_est_woutFR,breaks=30,xlim=c(0,60))
 abline(v=D,col="red",lwd=2)
 dev.off()
 
-pdf(file = "Estimators_BFGS_perturbedFP_zoom.pdf")
+pdf(file = "Estimators_L-BFGS-B_perturbedFP_zoom.pdf")
 par(mfrow=c(2,2))
 hist(C_est,breaks=30,xlim=c(0,10))
 abline(v=C,col="red",lwd=2)
-hist(D_est,breaks=30,xlim=c(0,10))
+hist(D_est,breaks=30,xlim=c(0,5))
 abline(v=D,col="red",lwd=2)
 
 hist(C_est_woutFR,breaks=30,xlim=c(0,10))
 abline(v=C,col="red",lwd=2)
-hist(D_est_woutFR,breaks=30,xlim=c(0,10))
+hist(D_est_woutFR,breaks=30,xlim=c(0,5))
 abline(v=D,col="red",lwd=2)
 dev.off()
+
+C_est
+D_est
+C_est_woutFR
+D_est_woutFR
 
 
 #######################################################################################
@@ -141,12 +141,13 @@ for (krep in 1:100){
   
   theta_start = c(runif(1,0.5,5),runif(1,0.01,10),runif(1,0.05,1),runif(1,0.1,1),runif(1,1,15),runif(1,0.05,1),runif(1,0.01,25),runif(1,0.01,5),runif(1,0.05,1))
   
-  p_opt<-try(optim(theta_start, logLik, y=data2,method="BFGS",hessian=T), silent=TRUE)
+  p_opt<-try(optim(theta_start, logLik, y=data2,method="L-BFGS-B",lower=0.01,hessian=T))
   if(is(p_opt, 'try-error'))
   {  
+    print(c("krep = ", paste(krep),"error"))
     C_est[krep] = NA
     D_est[krep] = NA 
-    break
+    next
   } else {
     C_est[krep] = p_opt$par[7]
     D_est[krep] = p_opt$par[8]
@@ -157,12 +158,13 @@ for (krep in 1:100){
   ### --------------- Without the KR data ------------------------
   theta_start = theta_start[-9]#one parameter less
   
-  p_opt<-try(optim(theta_start, logLik_FRwoutNoise, y=data2,method="BFGS",hessian=T), silent=TRUE)
+  p_opt<-try(optim(theta_start, logLik_FRwoutNoise, y=data2,method="L-BFGS-B",lower=0.01,hessian=T))
   if(is(p_opt, 'try-error'))
   {  
+    print(c("krep = ", paste(krep),"error"))
     C_est_woutFR[krep] = NA
     D_est_woutFR[krep] = NA 
-    break
+    next
   } else {
     C_est_woutFR[krep] = p_opt$par[7]
     D_est_woutFR[krep] = p_opt$par[8]
@@ -182,28 +184,33 @@ hist(D_est_woutFR)
 
 
 
-pdf(file = "Estimators_BFGS_noisyLC.pdf")
+pdf(file = "Estimators_L-BFGS-B_noisyLC.pdf")
 par(mfrow=c(2,2))
-hist(C_est,breaks=30,xlim=c(0,50))
+hist(C_est,breaks=30,xlim=c(0,20))
 abline(v=C,col="red",lwd=2)
-hist(D_est,breaks=30,xlim=c(0,60))
+hist(D_est,breaks=30,xlim=c(0,5))
 abline(v=D,col="red",lwd=2)
 
-hist(C_est_woutFR,breaks=30,xlim=c(0,50))
+hist(C_est_woutFR,breaks=30,xlim=c(0,20))
 abline(v=C,col="red",lwd=2)
-hist(D_est_woutFR,breaks=30)
-abline(v=D,col="red",lwd=2,xlim=c(0,60))
+hist(D_est_woutFR,breaks=30,xlim=c(0,5))
+abline(v=D,col="red",lwd=2)
 dev.off()
 
-pdf(file = "Estimators_BFGS_noisyLC_zoom.pdf")
+pdf(file = "Estimators_L-BFGS-B_noisyLC_zoom.pdf")
 par(mfrow=c(2,2))
-hist(C_est,breaks=30,xlim=c(0,10))
+hist(C_est,breaks=30,xlim=c(14,16))
 abline(v=C,col="red",lwd=2)
-hist(D_est,breaks=30,xlim=c(0,10))
+hist(D_est,breaks=30,xlim=c(0,1))
 abline(v=D,col="red",lwd=2)
 
-hist(C_est_woutFR,breaks=30,xlim=c(0,10))
+hist(C_est_woutFR,breaks=30,xlim=c(14,16))
 abline(v=C,col="red",lwd=2)
-hist(D_est_woutFR,breaks=30,xlim=c(0,10))
+hist(D_est_woutFR,breaks=30,xlim=c(0,1))
 abline(v=D,col="red",lwd=2)
 dev.off()
+
+C_est
+D_est
+C_est_woutFR
+D_est_woutFR
